2decbd374ddb07241ec9c159dedf2aa2ae561448
mworkfun <39076163+mworkfun@users.noreply.github.com>
2022-03-10 15:19:23 +0800

    Add files via upload

---
+++ mfrc522_i2c_pico/mfrc522.py
@@ -0,0 +1,22 @@
+import machine
+import time
+from mfrc522_i2c import mfrc522
+
+#i2c config
+addr = 0x28
+scl = 21
+sda = 20
+    
+rc522 = mfrc522(scl, sda, addr)
+rc522.PCD_Init()
+rc522.ShowReaderDetails()            # Show details of PCD - MFRC522 Card Reader details
+
+while True:
+    if rc522.PICC_IsNewCardPresent():
+        #print("Is new card present!")
+        if rc522.PICC_ReadCardSerial() == True:
+            print("Card UID:")
+            print(rc522.uid.uidByte[0 : rc522.uid.size])
+    #time.sleep(1)
+    
+    
